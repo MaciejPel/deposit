b9efce08b8b9cb7bb3236d3b72fc00274d0e6520
Maciej Pełczyński <maciejpelczynski0@gmail.com>
2020-08-11 23:42:54 +0200

    js beginning

---
+++ html & css/js_basic.js
@@ -0,0 +1,19 @@
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const UserList = document.querySelector("#users");
+myForm.addEventListener("submit", onSubmit);
+function onSubmit(e) {
+	e.preventDefault();
+	if (nameInput.value === "" || emailInput.value === "") {
+		msg.classList.add("error");
+		msg.innerHTML = "Please";
+		setTimeout(() => msg.remove(), 3000);
+	} else {
+		const li = document.createElement("li");
+		li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`));
+		UserList.appendChild(li);
+		nameInput.value = "";
+	}
+}
